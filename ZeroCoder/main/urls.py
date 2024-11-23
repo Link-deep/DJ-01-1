18e6ee00962dcff711714320b5fe0005df30bdce
Link-deep <link.deep@yandex.ru>
2024-11-23 21:48:21 +0500

    commit_1

--- ZeroCoder/main/urls.py
+++ ZeroCoder/main/urls.py
@@ -3,8 +3,11 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index),
-    path('contact/', views.contact),
-    path('test/', views.test),
-    path('data/', views.data),
+    path('', views.index, name='home'),
+    path('game/', views.game, name='games'),
+    path('suppliers/', views.suppliers, name='suppliers'),
+    path('contact/', views.contact, name='contact'),
+    path('privacy-policy/', views.privacy_policy, name='privacy'),
+    path('terms-of-service/', views.terms_of_service, name='terms'),
+
 ]
--- ZeroCoder/main/views.py
+++ ZeroCoder/main/views.py
@@ -5,16 +5,48 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("<h1>Hello, world. You're at the main index.</h1>")
+    return render(request, 'main/index.html', {'caption':"NINTENDO"})
 
 
 def contact(request):
-    return HttpResponse("<h1>Страница с контактами </h1>")
+    data = {
+        'caption': "NINTENDO"
+    }
+
+    return render(request, 'main/contact.html', data)
+
+def game(request):
+    data = {
+        'caption': "NINTENDO"
+    }
+
+    return render(request, 'main/game.html', data)
+
+
+
+def suppliers(request):
+    data = {
+        'caption': "NINTENDO"
+    }
+    return render(request, 'main/suppliers.html', data)
+
+
+def privacy_policy(request):
+    data = {
+        'caption': "NINTENDO"
+    }
+
+    return render(request, 'main/privacy-policy.html', data)
+
+def terms_of_service(request):
+    data = {
+        'caption': "NINTENDO"
+    }
+
+    return render(request, 'main/terms-of-service.html', data)
 
 
 def test(request):
     return HttpResponse("<h1>Тестовая страница </h1>")
 
 
-def data(request):
-    return HttpResponse("<h1>страница data </h1>")
